4c891648e4a63b2bdb0e6e63460a32017b47412f
thanhtudev00 <thanhtu.itweb@gmail.com>
2021-08-13 21:47:10 +0700

    hello man

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.scss';
 import { Fragment } from 'react';
 import Header from './containers/HeaderContainer';
 import Main from './containers/MainContainer/';
+import Footer from './containers/FooterContainer';
 
 function App() {
   return (
     <Fragment>
       <Header />
       <Main />
+      <Footer />
     </Fragment>
   );
 }
